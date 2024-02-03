a2ad9e9e5e8e62b68cff31d6a85739283c10b930
A-Gully <agallul@yahoo.com>
2024-02-03 22:03:09 +0000

    fix price route

--- server/src/controllers/history.ts
+++ server/src/controllers/history.ts
@@ -1,13 +1,21 @@
 import  { Request, Response } from 'express';
 import axios from 'axios';
+import { regionMapping } from '../dataMapping';
 
 
 export const priceRoute = async (req: Request, res: Response) => {
     const {region, sector, startDate, endDate} = req.params
-    const countryCode = {"EU": "KEUA", "NA": "KCCA"}
-    let apiUrl = "http://localhost:8000/api/v1/etf/historical?symbol="+countryCode.(region)+"&start_date=2021-01-01&end_date=2022-01-01"
-    const apiResponse = await axios.get(apiUrl);
-    res.send("Hello, Typescript with Express");
+    let apiUrl = `http://localhost:8000/api/v1/etf/historical?symbol=${regionMapping.region}&start_date=${startDate}&end_date=${endDate}`
+    try
+    {
+        const apiResponse = await axios.get(apiUrl);
+        res.send(apiResponse);
+    }
+    catch(error: any)
+    {
+        res.send({message: error.message})
+    }
+    
 }
 
 export const allocationsRoute = async (req: Request, res: Response) => {

e203b94044390074bd2cf7139faf0ce5a50ed5ce
Kunal Charn <charnkunal@gmail.com>
2024-02-03 21:41:24 +0000

    Data entry

--- server/src/controllers/predition.ts
+++ server/src/controllers/predition.ts
@@ -2,5 +2,7 @@ import  { Request, Response } from 'express';
 
 
 export const predictionAnalysis = async (req: Request, res: Response) => {
-    res.send("Hello, Typescript with Express");
+    const {region, sector, startDate, endDate} = req.params
+
+
 }
